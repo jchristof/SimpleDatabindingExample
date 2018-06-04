a4354e6e98255c8c551cc2516e5efe61db617c22
Jon Martin <jchristofmartin@gmail.com>
2018-06-03 19:03:10 -0500

    switched adapter to Int type

--- app/src/main/java/com/example/bindingframedimensions/bindingframedimensions/Binding.kt
+++ app/src/main/java/com/example/bindingframedimensions/bindingframedimensions/Binding.kt
@@ -4,8 +4,15 @@ import android.databinding.BindingAdapter
 import android.view.View
 
 @BindingAdapter("android:layout_width")
-fun setLayoutWidth(view: View, width: Float) {
+fun setLayoutWidth(view: View, width: Int) {
     val layoutParams = view.layoutParams
     layoutParams.width = width.toInt()
+    view.layoutParams = layoutParams
+}
+
+@BindingAdapter("android:layout_height")
+fun setLayoutHeight(view: View, height: Int) {
+    val layoutParams = view.layoutParams
+    layoutParams.width = height.toInt()
     view.layoutParams = layoutParams
 }
